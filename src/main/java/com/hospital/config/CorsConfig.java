3dcaf1f72277c57c94b34154451d2934a4460957
hitesh <hiteshhittu87@gmail.com>
2026-04-24 21:10:15 +0530

    added new cors

--- src/main/java/com/hospital/config/CorsConfig.java
+++ src/main/java/com/hospital/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // ✅ FIX: DO NOT use "*"
-        config.setAllowedOrigins(List.of("http://localhost:3001"));
+        config.setAllowedOrigins(List.of("http://localhost:3001","http://localhost:3000"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
