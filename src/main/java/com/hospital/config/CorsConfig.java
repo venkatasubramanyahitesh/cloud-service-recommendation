2cfcad55b331d26b9e472ba53c2e26d02d28583d
hitesh <hiteshhittu87@gmail.com>
2026-04-26 16:54:29 +0530

    fixed cors for deployed frontend

--- src/main/java/com/hospital/config/CorsConfig.java
+++ src/main/java/com/hospital/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // ✅ FIX: DO NOT use "*"
-        config.setAllowedOrigins(List.of("http://localhost:3001","http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3001","http://localhost:3000",  "https://cloud-frontend-5kqr.onrender.com"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
